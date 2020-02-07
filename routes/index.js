e5bc4cbbb86e82472daa5d27b11eed60e53c94e8
vhon0603 <41207326+vhon0603@users.noreply.github.com>
2020-02-07 10:20:59 -0800

    oopsies

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,8 @@
 exports.view = function(req, res){
   res.render('index');
 };
+
+function goToPage(url) {
+  console.log("hello")
+  popupWindow = window.open(url,'popUpWindow','height=500,width=500,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes');
+}
